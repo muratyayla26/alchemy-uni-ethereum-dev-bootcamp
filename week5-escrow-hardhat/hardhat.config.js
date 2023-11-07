865dca33a48d7c57f591cebad0775319d5ea7cdb
muratyayla <murat.yayla.26@gmail.com>
2023-11-07 17:09:24 +0300

    week 5 task its also deployed to goerli and tested there

---
+++ week5-escrow-hardhat/hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.17",
+  paths: {
+    artifacts: "./app/src/artifacts",
+  },
+};
+
